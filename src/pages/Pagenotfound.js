cafc3a7b13442a1f329bc2518f0c99e1765a8ff4
vinaykondi <140968666+vinaykondi@users.noreply.github.com>
2024-01-24 19:34:05 +0530

    Add files via upload

---
+++ src/pages/Pagenotfound.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+export default function Pagenotfound () {
+  return (
+    <Layout>
+      <h1>Page Not Found</h1>
+    </Layout>
+  );
+};
